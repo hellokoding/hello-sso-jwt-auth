0cfa7c8bccbbd8c7f34d9443264d1c44f2289904
Giau Ngo <giaunv@users.noreply.github.com>
2016-08-01 15:48:40 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Spring Boot Hello World Example with FreeMarker
+
+## Guide
+https://hellokoding.com/spring-boot-hello-world-example-with-freemarker/
+
+## What you'll need
+- JDK 1.7 or later
+- Maven 3 or later
+
+## Stack
+- Spring Boot
+- Java
+
+## Run
+`mvn spring-boot:run`
---
+++ src/main/java/com/hellokoding/sso/auth/JwtUtil.java
@@ -0,0 +1,21 @@
+package com.hellokoding.sso.auth;
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
+
+public class JwtUtil {
+    public static String generateToken(String signingKey, String subject) {
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+        JwtBuilder builder = Jwts.builder()
+                .setSubject(subject)
+                .setIssuedAt(now)
+                .signWith(SignatureAlgorithm.HS256, signingKey);
+
+        return builder.compact();
+    }
+}
